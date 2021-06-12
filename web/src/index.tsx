fe7878e81cbd25f3f7d16d76e9c227612009df6f
Arpit Bhalla <arpitbhalla2001@gmail.com>
2021-06-12 16:47:47 +0530

    Working

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './App';
-import { mergeStyles } from '@fluentui/react';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { mergeStyles } from "@fluentui/react";
+import reportWebVitals from "./reportWebVitals";
 
 // Inject some global styles
 mergeStyles({
-  ':global(body,html,#root)': {
+  ":global(body,html,#root)": {
     margin: 0,
     padding: 0,
-    height: '100vh',
+    height: "100vh",
   },
 });
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
