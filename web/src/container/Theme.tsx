b4589bb4eab49552ec0f970e3204bd997770a129
Arpit Bhalla <arpitbhalla2001@gmail.com>
2021-06-19 15:37:44 +0530

    Improved

--- web/src/container/Theme.tsx
+++ web/src/container/Theme.tsx
@@ -2,7 +2,9 @@ import * as React from "react";
 import { Provider as ThemeProvider } from "@fluentui/react-northstar";
 import { useAppSelector } from "core/hooks/redux";
 
-export const ThemeProviderWrapper: React.FunctionComponent = ({ children }) => {
+export const ThemeProviderWrapper: React.FunctionComponent<{
+  children: React.ReactNode;
+}> = ({ children }) => {
   const theme = useAppSelector((state) => state.themeReducer.theme);
 
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
