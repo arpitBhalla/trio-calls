18b562babdcb079f5b8ead57e30404c7a2f3cb5b
Arpit Bhalla <arpitbhalla2001@gmail.com>
2021-07-04 00:16:27 +0530

    :sparkles: Improved

---
+++ server/types.ts
@@ -0,0 +1,11 @@
+export type meeting = {
+  meetID?: string;
+  hostID?: string;
+  participantID?: string[];
+};
+
+export type user = {
+  email?: string;
+  password?: string;
+  userID?: string;
+};
