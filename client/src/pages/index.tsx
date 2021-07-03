a9ba9281a06324ed5d3bdafcae49de649dc05910
Arpit Bhalla <arpitbhalla2001@gmail.com>
2021-07-03 19:41:25 +0530

    :sparkles: Improved

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { Route } from "react-router-dom";
+
+type Props = {};
+
+const useStyles = makeStyles((theme) => ({
+  root: {},
+}));
+
+const App: React.FC<Props> = ({}) => {
+  const classes = useStyles();
+  return <></>;
+};
+export default App;
