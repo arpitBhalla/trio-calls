72b2fd4f3720f2e642644c6fc616672fdf8c7ef5
Arpit Bhalla <arpitbhalla2001@gmail.com>
2021-07-03 20:23:49 +0530

    :sparkles: Improved

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,12 +1,18 @@
 import * as React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import Avatar from "@material-ui/core/Avatar";
 import Box from "@material-ui/core/Box";
+import Chip from "@material-ui/core/Chip";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import Tab from "@material-ui/core/Tab";
 import Tabs from "@material-ui/core/Tabs";
 import NewMeet from "./components/NewMeet";
 import JoinMeet from "./components/JoinMeet";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { useAppSelector } from "core/hooks/redux";
 
 interface Props {}
 
@@ -19,40 +25,51 @@ const useStyles = makeStyles((theme) => ({
 
 const App: React.FC<Props> = () => {
   const classes = useStyles();
-
+  const { displayName } = useAppSelector(({ authReducer }) => authReducer);
   const [tabIndex, setTabIndex] = React.useState(0);
 
   return (
-    <Container maxWidth="md" className={classes.root}>
-      <Grid container spacing={1}>
-        <Grid item xs={7}>
-          <img
-            alt="Meeting"
-            src="https://www.gstatic.com/meet/user_edu_brady_bunch_light_81fa864771e5c1dd6c75abe020c61345.svg"
+    <>
+      <AppBar position="static" color="primary">
+        <Toolbar>
+          <Typography variant="h6">Microsoft Mini Teams</Typography>
+          <Chip
+            label={displayName}
+            avatar={<Avatar>{displayName[0]}</Avatar>}
           />
-        </Grid>
-        <Grid item xs={5}>
-          <Box
-            boxShadow="0px 0px 30px 1px rgb(214, 214, 214)"
-            p={3}
-            minHeight="50vh"
-          >
-            <Tabs
-              centered
-              value={tabIndex}
-              onChange={(e, n) => setTabIndex(n)}
-              variant="fullWidth"
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="md" className={classes.root}>
+        <Grid container spacing={1}>
+          <Grid item xs={7}>
+            <img
+              alt="Meeting"
+              src="https://www.gstatic.com/meet/user_edu_brady_bunch_light_81fa864771e5c1dd6c75abe020c61345.svg"
+            />
+          </Grid>
+          <Grid item xs={5}>
+            <Box
+              boxShadow="0px 0px 30px 1px rgb(214, 214, 214)"
+              p={3}
+              minHeight="50vh"
             >
-              <Tab label="Join with Code" />
-              <Tab label="Create New" />
-            </Tabs>
-            <Box pt={3} p={2}>
-              {tabIndex ? <NewMeet /> : <JoinMeet />}
+              <Tabs
+                centered
+                value={tabIndex}
+                onChange={(e, n) => setTabIndex(n)}
+                variant="fullWidth"
+              >
+                <Tab label="Join with Code" />
+                <Tab label="Create New" />
+              </Tabs>
+              <Box pt={3} p={2}>
+                {tabIndex ? <NewMeet /> : <JoinMeet />}
+              </Box>
             </Box>
-          </Box>
+          </Grid>
         </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </>
   );
 };
 export default App;
