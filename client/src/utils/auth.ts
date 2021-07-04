fe815279ff9682799af9e5c4423416694f11f41e
Arpit Bhalla <arpitbhalla2001@gmail.com>
2021-07-04 18:12:08 +0530

    :sparkles: Improved

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -5,6 +5,12 @@ type UserDetails = {
   email: string;
   UID: string;
 };
+export interface Meeting {
+  title: string;
+  hostID: string;
+  invitees: string[];
+  type: "public" | "private";
+}
 
 export const signIn = (email: string, password: string): Promise<UserDetails> =>
   new Promise(async (resolve, reject) => {
@@ -41,10 +47,28 @@ export const getProfile = (UID: string): Promise<UserDetails> =>
     } else reject();
   });
 
-export const newMeet = (UID: string): Promise<UserDetails> =>
+export const getMeet = (meetID: string): Promise<Meeting> =>
   new Promise(async (resolve, reject) => {
-    let res = await axiosFetch.post<UserDetails>("/newMeet", {
-      UID,
+    let res = await axiosFetch.post<Meeting>("/getMeet", {
+      meetID,
+    });
+    if (res.status === 200) {
+      resolve(res.data);
+    } else reject();
+  });
+
+export const newMeet = ({
+  hostID,
+  invitees,
+  title,
+  type,
+}: Meeting): Promise<Meeting> =>
+  new Promise(async (resolve, reject) => {
+    let res = await axiosFetch.post<Meeting>("/newMeet", {
+      hostID,
+      invitees,
+      title,
+      type,
     });
     if (res.status === 200) {
       resolve(res.data);
