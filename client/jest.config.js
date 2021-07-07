3e139818ded314ce432c77b6428b3febbd4d67bf
Arpit Bhalla <arpitbhalla2001@gmail.com>
2021-07-07 11:29:59 +0530

    [configs] :wrench: Add basic config

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,27 +1,19 @@
 module.exports = {
-  // The root of your source code, typically /src
-  // `<rootDir>` is a token Jest substitutes
-  roots: ["./src"],
-
+  roots: ["<rootDir>/src"],
+  preset: "ts-jest",
+  testEnvironment: "node",
   // Jest transformations -- this adds support for TypeScript
   // using ts-jest
   transform: {
     "^.+\\.(ts)$": "ts-jest",
   },
 
-  // Runs special logic, such as cleaning up components
-  // when using React Testing Library and adds special
-  // extended assertions to Jest
   setupFilesAfterEnv: [
     // "@testing-library/react/cleanup-after-each",
     "@testing-library/jest-dom/extend-expect",
   ],
 
-  // Test spec file resolution pattern
-  // Matches parent folder `__tests__` and filename
-  // should contain `test` or `spec`.
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
 
-  // Module file extensions for importing
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 };
